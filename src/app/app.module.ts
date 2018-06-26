84367c6f825c1c3cfd7dd86fd3c254b14a34771a
Zeno França Junior <zfrancajunior@gmail.com>
2018-06-26 15:43:57 -0300

    site published  with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,10 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CarouselComponent } from './carousel/carousel.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    CarouselComponent
   ],
   imports: [
     BrowserModule,
